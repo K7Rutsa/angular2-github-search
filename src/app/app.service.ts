8586cd95cfd79b7735912d3017699366946acb31
K RUTSA <k7rutsa@gmail.com>
2017-03-20 21:40:22 +0530

    error handler added

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class AppService {
@@ -14,13 +16,18 @@ export class AppService {
 
     getUsers(){
     	return this._http.get(`https://api.github.com/users/${this.username}?client_id='92fd2530d25fef115d87'&client_secret='b1b05a04fe5b9b990de638e2b19244c24deb0f91'`)
-    	.map(res => res.json());
+    	.map(res => res.json())
+        .catch(this.handleError)
     }
 
     getRepos(){
     	return this._http.get(`https://api.github.com/users/${this.username}/repos?client_id='92fd2530d25fef115d87'&client_secret='b1b05a04fe5b9b990de638e2b19244c24deb0f91'`)
-    	.map(res => res.json());
+    	.map(res => res.json())
+        .catch(this.handleError)
     }
 
+    private handleError(error){
+        return Observable.throw(error.json().error)
+    }
 
 }
--- src/app/home-component/home-component.component.ts
+++ src/app/home-component/home-component.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AppService } from '../app.service';
 
 @Component({
@@ -6,24 +6,27 @@ import { AppService } from '../app.service';
   templateUrl: './home-component.component.html',
   styleUrls: ['./home-component.component.css']
 })
-export class HomeComponentComponent {
+
+export class HomeComponentComponent implements OnInit{
   username: string;
   result: any;
   repos: any[] = [];
+  error: string;
 
-  constructor(private _appService: AppService) {
-  	this._appService.getUsers().subscribe((data) => this.result = data);
-  	this._appService.getRepos().subscribe((data) => this.repos = data);
-  }
+  constructor(private _appService: AppService) { }
 
   searchUser(){
   	this._appService.setUsername(this.username);
-  	this._appService.getUsers().subscribe((data) => this.result = data);
-  	this._appService.getRepos().subscribe((data) => this.repos = data);
+  	        this._appService.getUsers().subscribe((data) => this.result = data, (error) => this.error = error)
+  	this._appService.getRepos().subscribe((data) => this.repos = data, (error) => this.error = error)
+
   }
 
   
-
+  ngOnInit(){
+        this._appService.getUsers().subscribe((data) => this.result = data, (error) => this.error = error);
+        this._appService.getRepos().subscribe((data) => this.repos = data, (error) => this.error = error);
+  }
 
 
 
